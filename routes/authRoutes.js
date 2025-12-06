9fd2f836ca4efbccf9ceb443c9da036511dafeb1
Samuel Otieno <mksupresidentsaward@gmail.com>
2025-12-06 10:00:59 +0300

    Fix User model import casing for Linux/Render

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { body, validationResult } = require('express-validator');
 const jwt = require('jsonwebtoken');
-const User = require('../models/User');
+const User = require('../models/user');
 const { protectAdmin } = require('../middleware/authMiddleware');
 
 // Generate JWT with 1 hour expiration for auto-logout after inactivity
